80dd540481773d1b8afcf1203e442464d0962bd4
CasBex <123587431+CasBex@users.noreply.github.com>
2024-07-13 13:06:23 +0200

    Update src/FixMe.jl
    Co-authored-by: Orestis Ousoultzoglou <xlxs4@protonmail.ch>

--- src/FixMe.jl
+++ src/FixMe.jl
@@ -1,7 +1,7 @@
 module FixMe
 
 # Write your package code here.
-@warn """You've just loaded FixMe.jl. This package is not intended for productive use!
+@warn """You've just loaded FixMe.jl. This package is not intended for use in production!
 If you are using this as a learning tool you're doing it right though :)"""
 
 include("assignment1.jl")
