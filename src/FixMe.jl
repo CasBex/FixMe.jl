e320de0fd48dafb2d64520be387f525a5a94fce9
Cas Bex <cas.bex@kuleuven.be>
2024-07-13 11:58:14 +0200

    Add warning about productive use

--- src/FixMe.jl
+++ src/FixMe.jl
@@ -1,5 +1,7 @@
 module FixMe
 
 # Write your package code here.
+@warn """You've just loaded FixMe.jl. This package is not intended for productive use!
+If you are using this as a learning tool you're doing it right though :)"""
 
 end
